c41d13934c7bede58fb66bb10493d5e3ca1e0369
hamza-at-youcode <hamzayocude@gmail.com>
2021-11-18 01:09:29 +0100

    Create and test reading and displaying functions

--- sas-2020/main.c
+++ sas-2020/main.c
@@ -1,89 +1,51 @@
 #include<stdio.h>
 #include<stdlib.h>
-#define MAX 100
 
-char *elector[][2] = {
-    {"Hamza","HH12345"},
-    {"Fatima","E987763"},
-    {"Yassine","M234566"},
-    {"Jawad","A123456"},
-    {"Rida","BB87733"},
-    {"Outhman","N56683"},
-    {"Rihab","R6245634"},
-    {"ABD Ellah","S2354365"},
-    {"Yassmine","Y7635276"},
-    {"Arwa","AR26435634"}
-};
+typedef struct person{
+    int id;
+    char cin[15];
+    char fullName[30];
+}Person;
 
-char *cdd[][2] = {
-    {"1","MARWAN Moitawakil"},
-    {"2","SAID Zrghili"},
-    {"3","MANAL Tawfik"},
-    {"4","KARIM Soul"},
-    {"5","KHADIJA Soultan"}
-};
+void readPerson(Person *p){
+    printf("Enter the Id: ");
+    fflush(stdin);
+    scanf("%d",&p->id);
+    printf("Enter the CIN: ");
+    fflush(stdin);
+    scanf("%s",p->cin);
+    printf("Enter Full Name: ");
+    fflush(stdin);
+    scanf("%s",p->fullName);
+    printf("\n******************\n");
+}
 
-void displayCdd(char* e[][2],int len){
+void registerPersons(Person *p,int len){
+    for(int i=0;i<len;i++) {
+        printf("*** reading cdd %d *****\n",i+1);
+        readPerson(&p[i]);
+    };
+}
+
+void displayCdd(Person p[],int len){
     for (int i = 0; i < len; i++)
     {
         printf("\nCandidate %d:\n",i+1);
-        printf("   ID: %s\n",e[i][0]);
-        printf("   Name: %s",e[i][1]);
+        printf("   ID: %d\n",p[i].id);
+        printf("   Name: %s\n",p[i].fullName);
     }
-}
-
-void vote(char* e[][2],int st[][2],int len){
-    int voteFor;
-
-    for (int i = 0; i < len; i++)
-    {
-        system("cls");
-        
-        displayCdd(cdd,5);
-        
-        printf("\n\n*****************************\n");
-        printf("***** Elector ID: %s\n",e[i][1]);
-        printf("***** Full Name: %s\n",e[i][0]);
-        printf("*****************************\n");
-        printf("\nYou wante to vote for: ");
-
-        scanf("%d",&voteFor);
-        st[voteFor-1][1]+=1;
-    }
-}
-
-void round1(int nbrOfe,int nbrOfCdd,int stats[][2],int *l,int *n){
-    float bar = 15;
-    int len = 0;
-    for (int i = 0; i < nbrOfCdd; i++){
-        if ((stats[i][1]*100)/nbrOfe<bar)
-        {
-            *(l+i) = i+1; len++;
-        }
-    }
-    *n=len;
 }
 
 int main(){
-    int stats[MAX][2]; // It contains the id of candidate and nbr of votes
-    int numOfe = 10;
-    int numOfCdd = 5;
+    Person *c;
+    int l = 3;
+    c = (Person*)malloc(l*sizeof(Person));
+    printf("***** Registering candidates *****\n");
+    registerPersons(c,l);
+    printf("\n\n***** DISPLAYING CANDIDATES *****\n");
+    displayCdd(c,l);
 
-    for (int i = 0; i < numOfCdd; i++) {
-        stats[i][0] = i+1;
-        stats[i][1] = 0;
-    }
-
-    vote(elector,stats,numOfe);
-     
-    for (int i = 0; i < numOfCdd; i++)
-    {
-        printf("Cdd %d => %d\n",stats[i][0],stats[i][1]);
-    }
-
-    // Round 1
-    int *losers,n;
-    round1(numOfe,numOfCdd,stats,losers,&n);
-    printf("\nn = %d\n",n);
-    for (int i = 0; i < n; i++){printf(">%d\n",losers[i]);}
+    return 0;
 }
+
+
