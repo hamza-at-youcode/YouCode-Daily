1cafa924eec93e594fd0aa93edb63067de892f97
hamza-at-youcode <hamzayocude@gmail.com>
2021-11-18 01:40:01 +0100

    Create reset votes function

--- sas-2020/main.c
+++ sas-2020/main.c
@@ -53,6 +53,10 @@ void displayCdd(Candidate p[],int len){
     }
 }
 
+void resetVotes(Candidate *p,int len){
+    for(int i=0;i<len;i++) p->votes = 0;
+}
+
 int main(){
     Candidate *c;
     int l = 3;
